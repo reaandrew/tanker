5b6296bb31dde64d88852d2db0b6c59a95602e95
Andrew Rea <email@andrewrea.co.uk>
2013-03-06 09:01:14 +0000

    Updated the tests as I needed an event for creation, which turned into a good enough harness for what I needed.

--- tests/testAggregateRoot.js
+++ tests/testAggregateRoot.js
@@ -2,11 +2,11 @@ var util = require("util");
 var assert = require("assert");
 var uuid = require("node-uuid");
 
-function AggregateRoot(options) {
-    this.id = options.id;
+function AggregateRoot() {
+    this.id = null;
     this.uncommittedEvents = [];
     this.version = 0;
-}
+};
 
 AggregateRoot.prototype.applyChange = function(event) {
     event.version = ++this.version;
@@ -14,43 +14,42 @@ AggregateRoot.prototype.applyChange = function(event) {
     this["handle_" + event._eventName].apply(this, [event]);
 }
 
+function DummyAggregateRootCreatedEvent(options){
+    this._eventName = DummyAggregateRootCreatedEvent.name;
+    this.id = options.id;
+    this.name = options.name;
+};
+
 function NameChangedEvent(options) {
     this._eventName = NameChangedEvent.name;
     this.id = options.id;
     this.name = options.name;
-}
-
-function DummyAggregateRoot(options) {
-    AggregateRoot.call(this, options);
-}
-util.inherits(DummyAggregateRoot, AggregateRoot);
-
-DummyAggregateRoot.prototype.changeName = function(name) {
-    this.applyChange(new NameChangedEvent({
-        id: this.id,
-        name: name
-    }));
-}
-
-DummyAggregateRoot.prototype.handle_NameChangedEvent = function(event) {
-    this.name = event.name;
 };
 
-describe("Test Aggregate Root", function() {
+function DummyAggregateRoot(options) {
+    AggregateRoot.call(this);
+    this.applyChange(new DummyAggregateRootCreatedEvent({
+        id : options.id,
+        name : options.name
+    }));
+};
 
+util.inherits(DummyAggregateRoot, AggregateRoot);
+
+DummyAggregateRoot.prototype.handle_DummyAggregateRootCreatedEvent = function(event){
+    this.id = event.id;
+    this.name = event.name;
+}
+
+
+describe("Test Aggregate Root", function() {
     beforeEach(function() {
         this.id = uuid.v4();
+        this.name = "Something";
         this.testAgg = new DummyAggregateRoot({
-            id: this.id
+            id: this.id,
+            name : this.name
         });
-    });
-
-    it("Should initialize events", function() {
-        assert.equal(this.testAgg.uncommittedEvents.length, 0);
-    });
-
-    it("Should initialize version", function() {
-        assert.equal(this.testAgg.version, 0);
     });
 
     it("Should initialize id from parameters", function() {
@@ -58,19 +57,15 @@ describe("Test Aggregate Root", function() {
     });
 
     it("Should increment the version of the AggregateRoot after handling a known event", function() {
-        this.testAgg.changeName("Andy");
         assert.equal(this.testAgg.version, 1);
     });
 
     it("Should add the event being handled to the AggregateRoots list of uncommitted events", function(){
-        this.testAgg.changeName("Andy");
         assert.equal(this.testAgg.uncommittedEvents.length, 1);
     });
 
     it("Should assign the version of the AggregateRoot to the event being handled", function(){
-        this.testAgg.changeName("Andy");
         var event = this.testAgg.uncommittedEvents[0];
         assert.equal(event.version, 1);
     });
-
 });
