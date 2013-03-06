0e5bbf1b9f24400782fdcb689c1d0798b276266d
Andrew Rea <email@andrewrea.co.uk>
2013-03-06 08:14:01 +0000

    Added the event handling abstract and implementation function for a dummy event

--- tests/testAggregateRoot.js
+++ tests/testAggregateRoot.js
@@ -2,35 +2,63 @@ var util = require("util");
 var assert = require("assert");
 var uuid = require("node-uuid");
 
-function AggregateRoot(options){
+function AggregateRoot(options) {
     this.id = options.id;
     this.events = [];
     this.version = 0;
 }
 
-function DummyAggregateRoot(options){
-    AggregateRoot.call(this, options);
+AggregateRoot.prototype.applyChange = function(event) {
+    event.version = this.version++;
+    this["handle_" + event._eventName].apply(this, [event]);
 }
 
+function NameChangedEvent(options) {
+    this._eventName = NameChangedEvent.name;
+    this.id = options.id;
+    this.name = options.name;
+}
+
+function DummyAggregateRoot(options) {
+    AggregateRoot.call(this, options);
+}
 util.inherits(DummyAggregateRoot, AggregateRoot);
 
-describe("Test Aggregate Root", function(){
+DummyAggregateRoot.prototype.changeName = function(name) {
+    this.applyChange(new NameChangedEvent({
+        id: this.id,
+        name: name
+    }));
+}
 
-    before(function(){
+DummyAggregateRoot.prototype.handle_NameChangedEvent = function(event) {
+    this.name = event.name;
+};
+
+describe("Test Aggregate Root", function() {
+
+    before(function() {
         this.id = uuid.v4();
-        this.testAgg = new DummyAggregateRoot({id:this.id});
+        this.testAgg = new DummyAggregateRoot({
+            id: this.id
+        });
     });
 
-    it("Should initialize events", function(){
-        assert.equal(this.testAgg.events.length,0);
+    it("Should initialize events", function() {
+        assert.equal(this.testAgg.events.length, 0);
     });
 
-    it("Should initialize version", function(){
+    it("Should initialize version", function() {
         assert.equal(this.testAgg.version, 0);
     });
 
-    it("Should initialize id from parameters", function(){
+    it("Should initialize id from parameters", function() {
         assert.equal(this.testAgg.id, this.id);
+    });
+
+    it("Should apply a known event", function() {
+        this.testAgg.changeName("Andy");
+        assert.equal(this.testAgg.version, 1);
     });
 
 });
