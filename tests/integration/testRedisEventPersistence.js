d2285df1b9e92b7ef1c9399e77c3c63e29222919
Andrew Rea <email@andrewrea.co.uk>
2013-03-11 17:39:29 +0000

    Baking in a shell of the next tests I want to write

---
+++ tests/integration/testRedisEventPersistence.js
@@ -0,0 +1,16 @@
+describe("Test Redis Event Persistence", function(){
+
+    it("Should persist events", function(done){
+
+    });
+
+    it("Should retrieve all events for a given aggregate ID", function(done){
+
+    });
+
+    //Could not think of a better name for this test.  Need to ensure that we have optimistic concurrency.
+    it("Should be syncronized", function(done){
+
+    });
+
+});
